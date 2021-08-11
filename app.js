8b4c0c7daa2f4abd08cadcf9c4c82d7550526efc
qurtex <54555148+qurtex@users.noreply.github.com>
2021-08-11 17:39:46 +0300

    1

---
+++ app.js
@@ -0,0 +1,5 @@
+let getRoot = document.getElementById('root');
+let captcha = document.getElementById('qwidgets-captha');
+let ads = document.getElementById('qwidgets-ads');
+let video = document.getElementById('qwidgets-video');
+
